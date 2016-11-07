db0307d559278e8cd1978419001502a8ce7820f4
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-11-07 06:06:54 +0000

    All files

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
+if [ $# -ne 3 ]
+then
+echo "Requires the 3 parameter values for  \"auto-scaling-group-name\", \"launch-configuration-name\" and \"load-balancer-name\" to be passed to run this script"
+else
 echo "Retrieve instance id's attached to the auto-scaling-group"
 instance_id=$(aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].[InstanceId]')
 echo "After retrieving Instance ID"
 
 echo "Set desired capacity to 0"
-aws autoscaling set-desired-capacity --auto-scaling-group-name imageserverdemo --desired-capacity 0
+aws autoscaling set-desired-capacity --auto-scaling-group-name $1 --desired-capacity 0
 echo "Desired capacity set to 0"
 
 echo "Wait until the instances are terminated"
@@ -17,13 +20,13 @@ echo "Wait is completed and instances in Running state are terminated"
 sleep 30
 
 echo "Deleting auto-scaling-group"
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name imageserverdemo
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $1
 echo "AutoScaling group-name deleted"
 
 echo "Deleting launch-configuration-group"
-aws autoscaling delete-launch-configuration --launch-configuration-name imageserver
+aws autoscaling delete-launch-configuration --launch-configuration-name $2
 echo "Launch configuration deleted"
 
 echo "Deleting load-balancer"
-aws elb delete-load-balancer --load-balancer-name itmo-544-vh
+aws elb delete-load-balancer --load-balancer-name $3
 echo "Load-balancer deleted"
