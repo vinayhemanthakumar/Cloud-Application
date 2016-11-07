05da4050464fdcfc940bf17ff4bc4910e37dd310
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-11-07 05:34:46 +0000

    Creating RDS instnaces,sns,sqs and buckets

---
+++ install-app-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+echo "Requires 2 bucket names raw-vin and raw-hem to be passed as parameters to run this script"
+exit 0
+else
+echo "Creating DB instance"
+aws rds create-db-instance --db-instance-identifier vinaydb --db-instance-class db.t1.micro --engine MySQL --master-username kbryant --master-user-password arizzo44 --allocated-storage 20 --db-name school
+aws rds wait db-instance-available --db-instance-identifier vinaydb
+echo "DB instance created"
+echo "Creating SNS Topic"
+aws sns create-topic --name vh_worldseries
+echo "Creating SQS Queue"
+aws sqs create-queue --queue-name vh_cubs
+echo "Creating a Bucket"
+aws s3 mb s3://$1 --region us-west-2 
+aws s3 mb s3://$2 --region us-west-2 
+fi
