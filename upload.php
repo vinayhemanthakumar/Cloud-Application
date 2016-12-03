73c6dd188187cc7bd8722a5801393c311ec277dc
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-12-03 06:44:45 -0600

    Modified Upload.php

--- upload.php
+++ upload.php
@@ -1,22 +1,21 @@
-<?php 
-
+<?php
 session_start();
+$username=$_SESSION['userid'];
+echo "\n" . $_SESSION['userid'] ."\n";
 ?>
 <html>
-<head><title>Best Page Ever</title></head>
+<head><title>WELCOME TO UPLOAD PAGE</title></head>
 <body>
-<h1> World</h1>
-
-
+<h1>UPLOAD</h1>
 <form enctype="multipart/form-data" action="uploader.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
-
 <input type="file" name="userfile" />
+<br>
+<br>
+<br>
 <input type="submit" value="submit" />
+<a href="welcome.php?Back=1">Back</a>
 </form>
-
 </body>
 </html>
-
-
