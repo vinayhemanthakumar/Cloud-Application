2a2ff3d7dd85b65708484707b26f1cdb2744b916
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-12-04 17:07:33 +0000

    Adding one instance for Cron job

--- install-env.sh
+++ install-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]
+if [ $# -ne 8 ]
 then
-echo "Requires 6 parameters to be passed to run this script"
+echo "Requires 8 parameters to be passed to run this script"
 exit 0
 else
 echo "Creating $5 new instances"
@@ -17,11 +17,11 @@ aws ec2 wait instance-running --instance-ids $instance_id
 echo "After wait"
 
 echo "Before the Creation of Load Balancer"
-aws elb create-load-balancer --load-balancer-name itmo-544-vh --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-58436a3c
+aws elb create-load-balancer --load-balancer-name $7 --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-58436a3c
 echo "After the Creation of Load Balancer"
 
 echo "Register the instances created with the load balancer"
-aws elb register-instances-with-load-balancer --load-balancer-name itmo-544-vh --instances $instance_id
+aws elb register-instances-with-load-balancer --load-balancer-name $7 --instances $instance_id
 echo "After registering the instance with load balancer"
 
 echo "Creating Autoscaling Launch Configuration"
@@ -29,15 +29,18 @@ aws autoscaling create-launch-configuration --launch-configuration-name $4 --ima
 echo "After creating the Autoscaling Launch Configuration"
 
 echo "Creating Autoscaling Group"
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name imageserverdemo --launch-configuration $4 --availability-zone us-west-2a --max-size 5 --min-size 0 --desired-capacity 1
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $8 --launch-configuration $4 --availability-zone us-west-2a --max-size 5 --min-size 0 --desired-capacity 1
 echo "After creating the Autoscaling Launch Configuration"
 
 echo "Attaching created instances to auto scaling group"
-aws autoscaling attach-instances --instance-ids $instance_id --auto-scaling-group-name imageserverdemo
+aws autoscaling attach-instances --instance-ids $instance_id --auto-scaling-group-name $8
 echo "After attaching Instances to auto-scaling-group"
 
 echo "Attaching load balancer to auto scaling group"
-aws autoscaling attach-load-balancers --auto-scaling-group-name imageserverdemo --load-balancer-names itmo-544-vh
+aws autoscaling attach-load-balancers --auto-scaling-group-name $8 --load-balancer-names $7
 echo "After attaching the load balancer to auto-scaling-group"
 echo "Successful created instances"
+
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://cron.sh --count $5 --placement AvailabilityZone=us-west-2a --iam-instance-profile Name="$6"
+
 fi
