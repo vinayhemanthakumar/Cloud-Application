1bf4b53dbfca6a17a22734a4f0216a668c276a82
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-12-04 11:12:12 +0000

    Fetching the Auto-scaling, Launch-configuration, Load-balancer

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
-then
-echo "Requires the 3 parameter values for  \"auto-scaling-group-name\", \"launch-configuration-name\" and \"load-balancer-name\" to be passed to run this script"
-else
 echo "Retrieve instance id's attached to the auto-scaling-group"
 instance_id=$(aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].[InstanceId]')
 echo "After retrieving Instance ID"
 
+echo -e "Collect auto-scaling-group-name"
+autoscalingGrpName=$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]')
+
+echo -e "Collect launch-configuration-name"
+launchConfigName=$(aws autoscaling describe-launch-configurations --query 'LaunchConfigurations[*].[LaunchConfigurationName]')
+
+echo -e "Collect load-balancer-name"
+loadBalancerName=$(aws elb describe-load-balancers --query 'LoadBalancerDescriptions[*].[LoadBalancerName]')
+
 echo "Set desired capacity to 0"
-aws autoscaling set-desired-capacity --auto-scaling-group-name $1 --desired-capacity 0
+aws autoscaling set-desired-capacity --auto-scaling-group-name $autoscalingGrpName --desired-capacity 0
 echo "Desired capacity set to 0"
 
 echo "Wait until the instances are terminated"
@@ -20,15 +25,15 @@ echo "Wait is completed and instances in Running state are terminated"
 sleep 30
 
 echo "Deleting auto-scaling-group"
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $1
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $autoscalingGrpName
 echo "AutoScaling group-name deleted"
 
 echo "Deleting launch-configuration-group"
-aws autoscaling delete-launch-configuration --launch-configuration-name $2
+aws autoscaling delete-launch-configuration --launch-configuration-name $launchConfigName
 echo "Launch configuration deleted"
 
 echo "Deleting load-balancer"
-aws elb delete-load-balancer --load-balancer-name $3
+aws elb delete-load-balancer --load-balancer-name $loadBalancerName
 echo "Load-balancer deleted"
 
 #delete-database instance
