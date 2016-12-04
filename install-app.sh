a8562c00aa8c29c7651ba06c42abba2fb2c3744e
vinayhemanthakumar <vhemanth@hawk.iit.edu>
2016-12-04 19:34:00 +0000

    Adding sql-client and restore to script

--- install-app.sh
+++ install-app.sh
@@ -8,7 +8,7 @@ echo "Hello" > /home/ubuntu/hello.txt
 
 run-one-until-success sudo apt-get update -y
 
-run-one-until-success sudo apt-get install -y apache2 php-xml php php-mysql curl zip unzip git php7.0-xml libapache2-mod-php php7.0-cli php-gd
+run-one-until-success sudo apt-get install -y apache2 php-xml php php-mysql mysql-client curl zip unzip git php7.0-xml libapache2-mod-php php7.0-cli php-gd
 
 export COMPOSER_HOME=/root && /usr/bin/composer.phar self-update 1.0.0-alpha11
 curl -sS https://getcomposer.org/installer | php
@@ -46,6 +46,8 @@ sudo cp /vinayhemanthakumar/admin.php /var/www/html
 
 sudo cp /vinayhemanthakumar/edit.php /var/www/html
 
-sudo cp /vinayhemanthakumar/dbbackup.php /var/www/html
+sudo cp /vinayhemanthakumar/backup.php /var/www/html
+
+sudo cp /vinayhemanthakumar/restore.php /var/www/html
 
 sudo cp /vinayhemanthakumar/IIT-logo.png /var/www/html
